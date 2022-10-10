e302127060a0612675038bb8388f2d19fcb9f519
Jasur Fozilov <jasurfozilov42@gmail.com>
2022-10-10 20:43:51 +0500

    modified urls.py

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,9 @@ from books import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',views.book_list,name='book_list'),
-    path('books/create/',views.book_create,name='book_create'),
-    path('books/<int:pk>/update/',views.book_update,name='book_update'),
-    path('books/<int:pk>/delete/',views.book_delete,name='book_delete'),
+    path('',views.book_list,name='book_list'),
+    path('create/',views.book_create,name='book_create'),
+    path('<int:pk>/update/',views.book_update,name='book_update'),
+    path('<int:pk>/delete/',views.book_delete,name='book_delete'),
 
 ]
